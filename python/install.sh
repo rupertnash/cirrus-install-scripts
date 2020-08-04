66994b35435019b9bf10d76cbc528169405f63da
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-08-04 09:56:19 +0100

    make python install upgrade pip and create python symlink

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,17 @@
-../generic/cmake/install.sh
+#!/bin/bash
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    echo "Don't source me"
+    return 1
+fi
+set -e
+thisdir=$(readlink -f $(dirname $BASH_SOURCE))
+. $thisdir/env.sh
+
+pushd $build_dir_name
+make install
+popd
+
+pushd $prefix/bin
+
+./pip3 install --upgrade pip
+ln -s python3 python
