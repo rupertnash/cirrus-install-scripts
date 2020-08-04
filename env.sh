441966b7779e4a698b0f10d92c8cdcd9a27420e1
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-08-04 09:20:24 +0100

    move location to global shared not project shared

--- env.sh
+++ env.sh
@@ -3,12 +3,12 @@ if [ -z "$INSTALLER_ENV_SH" ]; then
 
     installer_dir=$(readlink -f $(dirname $BASH_SOURCE))
 
-    export base_dir=/lustre/home/z04/shared/nash
+    export base_dir=/lustre/home/shared/nash
     export app_dir=$base_dir/sw
     export mod_dir=$base_dir/modules
     if [[ ! "$MODULEPATH" =~ ".*$mod_dir.*" ]]; then
 	module use $mod_dir
     fi
 
-    export make_parallelism=8
+    export make_parallelism=16
 fi
